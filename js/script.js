7fd9fcaf0ce8c3d17c5b7c7e8bc1c321d4bace13
ImanolVL21 <imanol15villanueva@gmail.com>
2024-03-31 00:12:14 -0600

    Voy a subir mi pagina web

---
+++ js/script.js
@@ -0,0 +1,168 @@
+const app = document.getElementById('typewriter');
+
+const typewriter=new Typewriter (app, {
+    loop: true,
+    delay: 200
+});
+
+typewriter
+ .typeString('BAD BUNNY')
+ .pauseFor(200)
+ .start();
+
+const app2 = document.getElementById('typewriter2');
+
+const typewriter2=new Typewriter (app2, {
+    loop: true,
+    delay: 200
+});
+
+typewriter2
+ .typeString('TRAVIS SCOTT')
+ .pauseFor(200)
+ .start();
+
+
+
+ const app3 = document.getElementById('typewriter3');
+
+ const typewriter3=new Typewriter (app3, {
+     loop: true,
+     delay: 200
+ });
+ 
+ typewriter3
+  .typeString('1. CRISTIANO RONALDO')
+  .pauseFor(200)
+  .start();
+
+
+
+const app4 = document.getElementById('typewriter4');
+
+const typewriter4=new Typewriter (app4, {
+    loop: true,
+    delay: 200
+});
+
+typewriter4
+ .typeString('2. MESSI')
+ .pauseFor(200)
+ .start();
+
+
+ const app5 = document.getElementById('typewriter5');
+
+ const typewriter5=new Typewriter (app5, {
+     loop: true,
+     delay: 200
+ });
+ 
+ typewriter5
+  .typeString('3. LA CHAMPIONS')
+  .pauseFor(200)
+  .start();
+
+
+const app6 = document.getElementById('typewriter6');
+
+  const typewriter6=new Typewriter (app6, {
+      loop: true,
+      delay: 200
+  });
+  
+  typewriter6
+   .typeString('IMANOL_WAR')
+   .pauseFor(200)
+   .start();
+
+
+   const app7 = document.getElementById('typewriter7');
+
+  const typewriter7=new Typewriter (app7, {
+      loop: true,
+      delay: 200
+  });
+  
+  typewriter7
+   .typeString('MI VIDA ACADEMICA')
+   .pauseFor(200)
+   .start();
+
+
+   const app8 = document.getElementById('typewriter8');
+
+  const typewriter8=new Typewriter (app8, {
+      loop: true,
+      delay: 200
+  });
+  
+  typewriter8
+   .typeString('FACULTAD DE INGENIERÍA')
+   .pauseFor(200)
+   .start();
+
+
+   const app9 = document.getElementById('typewriter9');
+
+   const typewriter9=new Typewriter (app9, {
+       loop: true,
+       delay: 200
+   });
+   
+   typewriter9
+    .typeString('PROYECTO 1')
+    .pauseFor(200)
+    .start();
+
+
+    const app10 = document.getElementById('typewriter10');
+
+    const typewriter10=new Typewriter (app10, {
+        loop: true,
+        delay: 200
+    });
+    
+    typewriter10
+     .typeString('PROYECTO 2')
+     .pauseFor(200)
+     .start();
+
+     const app11 = document.getElementById('typewriter11');
+
+    const typewriter11=new Typewriter (app11, {
+        loop: true,
+        delay: 200
+    });
+    
+    typewriter11
+     .typeString('PROYECTO 3')
+     .pauseFor(200)
+     .start();
+
+
+     const app12 = document.getElementById('typewriter12');
+
+     const typewriter12=new Typewriter (app12, {
+         loop: true,
+         delay: 200
+     });
+     
+     typewriter12
+      .typeString('MEGA GENGAR')
+      .pauseFor(200)
+      .start();
+
+
+      const app13 = document.getElementById('typewriter13');
+
+      const typewriter13=new Typewriter (app13, {
+          loop: true,
+          delay: 200
+      });
+      
+      typewriter13
+       .typeString('YO VISTO ASI')
+       .pauseFor(200)
+       .start();
+
